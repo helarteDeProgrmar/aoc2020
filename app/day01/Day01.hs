d8b9b396d457ae953fe5bdc79fd60c88801f62a2
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-06 18:06:56 +0100

    feat: upload day 1

--- app/day01/Day01.hs
+++ app/day01/Day01.hs
@@ -4,8 +4,8 @@ import Data.List
 main :: IO Int
 main = giveResult1 2020 =<< readNumbers
 
---main2 :: IO Int
---main2 = giveResult2 =<< readNumbers
+main2 :: IO Int
+main2 = giveResult2 =<< readNumbers
 
 readNumbers :: IO [Int]
 readNumbers = do
@@ -21,7 +21,11 @@ giveResult1 criteria (x:xs) = do
         Just c  -> return (c * x)
         Nothing -> giveResult1 criteria xs
 
---giveResult2 :: [Int] -> IO Int
---giveResult2 [] = return 0
---giveResult2 [_] = return 0
--- giveResult2 (x1:x2:xs)
+giveResult2 :: [Int] -> IO Int
+giveResult2 [] = return 0
+giveResult2 [_] = return 0
+giveResult2 (x1:x2:xs) = do
+    result <- giveResult1 (2020 - x1) (x2:xs)
+    case result of
+        0 -> giveResult2 (x2:xs)
+        a -> return (a * x1)
