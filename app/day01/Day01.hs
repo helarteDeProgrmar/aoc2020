90bb195aa9654d0f5612f1c9fdb88f54ff1f91d6
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-06 17:46:49 +0100

    feat: upload first part of day 1

--- app/day01/Day01.hs
+++ app/day01/Day01.hs
@@ -1,9 +1,27 @@
-import System.IO (readFile)
+import System.IO
+import Data.List
 
-main :: IO ()
-main = do
-    -- Lee el contenido del archivo "ejemplo.txt" y lo guarda en contents
-    contents <- readFile "ejemplo.txt"
-    
-    -- Imprime el contenido en pantalla
-    putStrLn contents
+main :: IO Int
+main = giveResult1 2020 =<< readNumbers
+
+--main2 :: IO Int
+--main2 = giveResult2 =<< readNumbers
+
+readNumbers :: IO [Int]
+readNumbers = do
+    contents <- readFile "app/day01/input1"
+    let result = map read (lines contents)
+    return result
+
+giveResult1 :: Int -> [Int] -> IO Int
+giveResult1 _ [] = return 0
+giveResult1 criteria (x:xs) = do
+    let complementary = find (\y -> x + y == criteria) xs
+    case complementary of
+        Just c  -> return (c * x)
+        Nothing -> giveResult1 criteria xs
+
+--giveResult2 :: [Int] -> IO Int
+--giveResult2 [] = return 0
+--giveResult2 [_] = return 0
+-- giveResult2 (x1:x2:xs)
