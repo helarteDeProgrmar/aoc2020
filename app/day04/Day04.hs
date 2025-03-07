c93d187edf97204401c5f0db84326c25a9da3e8d
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-07 17:04:04 +0100

    feat: upload first part of day 4

--- app/day04/Day04.hs
+++ app/day04/Day04.hs
@@ -9,4 +9,5 @@ resolve1 :: [[String]] -> Int
 resolve1 list = length [ x | x <- list, correct_passport x]
 
 correct_passport :: [String] -> Bool
-correct_passport list = (length list) == 8 || ((length list) == 7)
+correct_passport list = (length list) == 8 
+    || ((length list) == 7 &&  not (elem "cid" ( map (\x -> head (splitOn ":" x)) list )))
