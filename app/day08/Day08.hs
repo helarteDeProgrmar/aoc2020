816cceeda141a25d4b08e70e19887b7fb6239c47
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-11 17:05:17 +0100

    feat: upload completed day 8

--- app/day08/Day08.hs
+++ app/day08/Day08.hs
@@ -1,5 +1,3 @@
-import Data.List.Split (splitOn)
-
 readInput :: IO [(String, Int, Int)]
 readInput = do
     contents <- readFile "app/day08/input"
@@ -7,12 +5,12 @@ readInput = do
     return (map (\l -> let [c,n] = words l in (c, read (clean n) :: Int,0)) differentLines)
 
 clean :: String -> String
-clean "" = ""
 clean ('+':xs) = xs
 clean s = s
 
 run :: [(String, Int, Int)] -> Int -> Int -> Int
 run program pc acc
+    | pc >= length program = acc
     | i >= 1 = acc
     | comand == "nop" = run updatedProgram (pc+1) acc
     | comand == "jmp" = run updatedProgram (pc+mount) acc
@@ -21,4 +19,23 @@ run program pc acc
         (before, rest) = splitAt pc program
         (comand, mount, i) = head rest
         updatedProgram = before ++ [(comand, mount, i + 1)] ++ tail rest
-run _ _ _ = -666
+
+listIndex program = [x | (x, (comand,_,_)) <- zip [0..] program, comand == "nop" || comand =="jmp"]
+
+run2 :: [(String, Int, Int)] -> Int -> Int -> [Int] -> Int
+run2 program pc acc list
+    | pc >= length program = acc
+    | i >= 1 = run2 (cleanP program) 0 0 (tail list)
+    | h == pc && comand == "nop" = run2 updatedProgram (pc+mount) acc list
+    | h == pc && comand == "jmp" = run2 updatedProgram (pc+1) acc list
+    | comand == "nop" = run2 updatedProgram (pc+1) acc list
+    | comand == "jmp" = run2 updatedProgram (pc+mount) acc list
+    | comand == "acc" = run2 updatedProgram (pc+1) (acc+mount) list
+    where 
+        (before, rest) = splitAt pc program
+        (comand, mount, i) = head rest
+        updatedProgram = before ++ [(comand, mount, i + 1)] ++ tail rest
+        h = head list
+
+cleanP :: [(String, Int, Int)] -> [(String, Int, Int)]
+cleanP = map (\(c, m, _) -> (c, m, 0))
