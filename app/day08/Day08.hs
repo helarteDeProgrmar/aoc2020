45799dfe3a90d5cead6d15128cd721334fe6c6e1
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-11 16:07:05 +0100

    feat: add the first part of day 8

--- app/day08/Day08.hs
+++ app/day08/Day08.hs
@@ -1,7 +1,24 @@
 import Data.List.Split (splitOn)
 
-readInput :: IO []
+readInput :: IO [(String, Int, Int)]
 readInput = do
     contents <- readFile "app/day08/input"
-    let diferentsLines = lines contents
-    return (map transformLine diferentsLines)
+    let differentLines = lines contents
+    return (map (\l -> let [c,n] = words l in (c, read (clean n) :: Int,0)) differentLines)
+
+clean :: String -> String
+clean "" = ""
+clean ('+':xs) = xs
+clean s = s
+
+run :: [(String, Int, Int)] -> Int -> Int -> Int
+run program pc acc
+    | i >= 1 = acc
+    | comand == "nop" = run updatedProgram (pc+1) acc
+    | comand == "jmp" = run updatedProgram (pc+mount) acc
+    | comand == "acc" = run updatedProgram (pc+1) (acc+mount)
+    where 
+        (before, rest) = splitAt pc program
+        (comand, mount, i) = head rest
+        updatedProgram = before ++ [(comand, mount, i + 1)] ++ tail rest
+run _ _ _ = -666
