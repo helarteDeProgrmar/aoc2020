1fb77acaeb5ef6f3d2aad8719514e2f295b37f85
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-11 14:19:14 +0100

    feat: upload day 7

--- app/day07/Day07.hs
+++ app/day07/Day07.hs
@@ -1,19 +1,48 @@
 import Data.List.Split (splitOn)
-import Data.List (intersect)
 
 readInput :: IO [(String, [(String, Int)])]
 readInput = do
     contents <- readFile "app/day07/input"
-    diferentsLines = lines contents
-    return map transformLine diferentsLines
+    let diferentsLines = lines contents
+    return (map transformLine diferentsLines)
 
 transformLine :: String -> (String, [(String, Int)])
 transformLine line = 
-    let [firts, second]= splitOn " bags contain " in
+    let [firts, second] = splitOn " bags contain " line in
     (firts, transSecond second)
 
-transSecond :: String -> [String, Int]
+transSecond :: String -> [(String, Int)]
 transSecond s =
-    let s' = take 1 (splitOn "." s)
-    branchs = split ", " s'
+    let s' = head (splitOn "." s)
+        control = head (words s')
+        branchs = splitOn ", " s' in
+    if length branchs == 1 && control == "no" then
+        []
+    else
+        transSecond' branchs
 
+transSecond' :: [String] -> [(String, Int)]
+transSecond' [] = []
+transSecond' (x:xs) = 
+    let ws = words x in
+    (ws !! 1 ++ " " ++ ws !! 2, read $ ws !! 0) : transSecond' xs
+
+resolve1 :: [(String, [(String, Int)])] -> Int
+resolve1 list = 
+    let dependencies = [(x,y) | (x, y') <- list, y <- [n | (n,_) <- y']] in
+    countColors ["shiny gold"] dependencies []
+
+countColors :: [String] -> [(String, String)] -> [String] -> Int
+countColors [] _ list = length list
+countColors (x:xs) depen list =
+    let addToList = [n | (n,x') <- depen, x'==x, not (n `elem` list)]
+        addKernel = [n | n <- addToList, not(n `elem` xs)] in
+    countColors (addKernel ++ xs) depen (list ++ addToList)
+
+resolve2 :: [(String, [(String, Int)])] -> Int
+resolve2 list = countBags list ("shiny gold", 1) - 1
+
+countBags :: [(String, [(String, Int)])] -> (String, Int) -> Int
+countBags depen (s, n) =
+    let (children:_) = [ count | (name, count) <- depen, name==s] in
+    n + n*(foldl (+) 0 (map (countBags depen) children))
