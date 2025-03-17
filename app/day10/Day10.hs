686b97a1898db7bc762b3114c36e5eae9938cd30
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-17 16:52:39 +0100

    feat: finished day 10

--- app/day10/Day10.hs
+++ app/day10/Day10.hs
@@ -1,9 +1,16 @@
 import Data.List (sort)
 import Data.List.Split (splitOn)
 
+main :: IO (Int, Int)
+main = do
+    input <- readInput
+    let s1 = resolve1 input
+    let s2 = resolve2 input
+    return (s1,s2)
+
 readInput :: IO [Int]
 readInput = do
-    content <- readFile "app/day10/input"
+    content <- readFile "app/day10/input2"
     return (map read $ lines content)
 
 resolve1 :: [Int] -> Int
@@ -25,15 +32,17 @@ apears (_:xs) (a,b,c) = apears xs (a,b,c+1)
 resolve2 :: [Int] -> Int
 resolve2 l = 
     let sorted = sort l
-        joltages = zipWith (-) sorted (0:sorted) in
-        product $ map combine $ map length $ splitOn [3] joltages 
+        posiblePermuts = map (\x -> x-1) $ map length $ splitOn [3] $ zipWith (-) sorted (0:sorted) in
+        product $ map combine posiblePermuts
 
 combine :: Int -> Int
 -- combine 0 = 1
 -- combine 1 = 2
 -- combine 2 = 4
 -- combine n = combine (n-1) + combine (n-2) + combine (n-3)
-combine n = combines !! n
+combine n
+    | n >= 0 = combines !! n
+    | otherwise = 1
 
 combines :: [Int]
 combines = 1 : 2 : 4: zipWith3 sum3 combines (tail combines) (drop 2 combines)
